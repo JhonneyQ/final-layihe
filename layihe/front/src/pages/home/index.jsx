e0d8c6306c11443e513799155e9d3874df6170c0
JhonneyQ <kananqadirov2005@gmail.com>
2025-02-06 19:18:19 +0400

    fnishing grid part in home page

--- layihe/front/src/pages/home/index.jsx
+++ layihe/front/src/pages/home/index.jsx
@@ -27,11 +27,56 @@ const Home = () => {
                 </div>
             </section>
             <section className='sec2'>
-                <div className='photo'>
-                    <img className='img' src="https://media-hosting.imagekit.io//0eeb01c039634a47/Warwick_6-Photoroom.png?Expires=1833376049&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=nPUHDSTbnmizqPARZY1rZ8pvZD1d-DGfvwmo~w89d6uDcglsFVpqXvGEUDQZYWXpzKPY~~UjjjQuYvkCcge~8wKwXpSkLw3mo2ekrWnwO50XzHgSK1GZSsDtP-j49iOCyAv2~2GuqwY6LkPRzjta2af43BpOp5DAUPpOQAovGAqNAeipogfzSlKfjmYN-MTuG2GevaOCLUXc3~nJ2J6jYIahUoG0ftDr4DVWrCSruS8bklCbpKUfnhb9ybL0I4YODUEiD6~rQZZaza4VjTuNX-8Sp3ocZGFbF6LaPpaF~phVW5qvrm4gYfjtD9PxZ251rq57X55hX1dhWz0dM2DBDg__" alt="" />
-                    <div className='pho'>
-                        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/68704982-1681-4007-96a8-e86b9ddbfe82/db4on8c-be21e6d9-2a95-4bd0-be11-71265449bb90.png/v1/fill/w_1192,h_670/firefang_warwick_render_by_zactheacorn_db4on8c-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvNjg3MDQ5ODItMTY4MS00MDA3LTk2YTgtZTg2YjlkZGJmZTgyXC9kYjRvbjhjLWJlMjFlNmQ5LTJhOTUtNGJkMC1iZTExLTcxMjY1NDQ5YmI5MC5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.I5fy8lgaCY8siTpecchgH7R80HSttoCHk5BSDc8J6w8" alt="" />
+                <div className='cards'>
+                    <div className='b'>
+                        <div className='emp'>
+
+                        </div>
+                        <div className='evelyn '>
+
+                        </div>
+                        <div className='photo'>
+                            <img className='img' src="https://media-hosting.imagekit.io//0eeb01c039634a47/Warwick_6-Photoroom.png?Expires=1833376049&Key-Pair-Id=K2ZIVPTIP2VGHC&Signature=nPUHDSTbnmizqPARZY1rZ8pvZD1d-DGfvwmo~w89d6uDcglsFVpqXvGEUDQZYWXpzKPY~~UjjjQuYvkCcge~8wKwXpSkLw3mo2ekrWnwO50XzHgSK1GZSsDtP-j49iOCyAv2~2GuqwY6LkPRzjta2af43BpOp5DAUPpOQAovGAqNAeipogfzSlKfjmYN-MTuG2GevaOCLUXc3~nJ2J6jYIahUoG0ftDr4DVWrCSruS8bklCbpKUfnhb9ybL0I4YODUEiD6~rQZZaza4VjTuNX-8Sp3ocZGFbF6LaPpaF~phVW5qvrm4gYfjtD9PxZ251rq57X55hX1dhWz0dM2DBDg__" alt="" />
+                            <div className='pho'>
+                                <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/68704982-1681-4007-96a8-e86b9ddbfe82/db4on8c-be21e6d9-2a95-4bd0-be11-71265449bb90.png/v1/fill/w_1192,h_670/firefang_warwick_render_by_zactheacorn_db4on8c-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9OTAwIiwicGF0aCI6IlwvZlwvNjg3MDQ5ODItMTY4MS00MDA3LTk2YTgtZTg2YjlkZGJmZTgyXC9kYjRvbjhjLWJlMjFlNmQ5LTJhOTUtNGJkMC1iZTExLTcxMjY1NDQ5YmI5MC5wbmciLCJ3aWR0aCI6Ijw9MTYwMCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.I5fy8lgaCY8siTpecchgH7R80HSttoCHk5BSDc8J6w8" alt="" />
+                            </div>
+
+                        </div>
+
                     </div>
+                    <div className='c'>
+                        <div className='d'>
+                            <div className='runes'>
+
+                            </div>
+                            <div className='rune'>
+
+                            </div>
+                        </div>
+                        <div className='emp2'>
+
+                        </div>
+
+                    </div>
+                    <div className='e'>
+                        <div className='emp'>
+
+                        </div>
+                        <div className='flower'>
+
+                        </div>
+                    </div>
+                    <div className='f'>
+                        <div className='art'>
+
+                        </div>
+                        <div className='ryze'>
+                            
+                        </div>
+                    </div>
+
+
+
 
                 </div>
             </section>
