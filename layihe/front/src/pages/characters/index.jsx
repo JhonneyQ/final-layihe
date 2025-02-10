33d6f6168a28bc41747f98329faaa23937c7f294
JhonneyQ <kananqadirov2005@gmail.com>
2025-02-10 17:31:18 +0400

    karakterler dizayb bir hissesi

--- layihe/front/src/pages/characters/index.jsx
+++ layihe/front/src/pages/characters/index.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import "./index.scss"
 
 const Characters = () => {
   return (
-    <div>characters</div>
+    <section className='ch'>
+      <div className='container'>
+        <div className='all'>
+          <div className='side'>
+            <input className='search' />
+            <div>
+              <div className='filt'> 
+                <input type='checkbox' className='assa' />
+                <label htmlFor='assa'>assassin</label>
+              </div>
+              <div className='filt'>
+                <input type='checkbox' className='assa' />
+                <label htmlFor='assa'>assassin</label>
+              </div>
+              <div className='filt'>
+                <input type='checkbox' className='assa' />
+                <label htmlFor='assa'>assassin</label>
+              </div>
+              <div className='filt'>
+                <input type='checkbox' className='assa' />
+                <label htmlFor='assa'>assassin</label>
+              </div>
+              <div className='filt'>
+                <input type='checkbox' className='assa' />
+                <label htmlFor='assa'>assassin</label>
+              </div>
+            </div>
+          </div>
+          <div className='champs'>
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
