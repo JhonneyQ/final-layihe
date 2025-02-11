44750f07a4102336d5bf502b34c2a9ad8808cfbe
JhonneyQ <kananqadirov2005@gmail.com>
2025-02-11 20:42:47 +0400

    karakter sehifesi 70% hazirlanmasi

--- layihe/front/src/pages/characters/index.jsx
+++ layihe/front/src/pages/characters/index.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./index.scss"
+import axios from "axios"
 
 const Characters = () => {
+  const [champs, setChamps] = useState([])
+
+  const getData = async () => {
+    try {
+      const res = await axios("http://localhost:8080/api/champions/")
+      setChamps(res.data.data)
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+
   return (
     <section className='ch'>
       <div className='container'>
         <div className='all'>
           <div className='side'>
-            <input className='search' />
-            <div>
-              <div className='filt'> 
+            <div className='filtt'>
+              <input className='search' type="search" autocomplete="off" placeholder='search'/>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="-20 -20 1000 150">
+                <path class="path" fill="none" d="M924.4 85.2c-19.5 19.5-50.8 19.7-70.3 0-19.3-19.4-19.3-51 .3-70.6 19.5-19.5 51-19.4 70.6 0 19.3 19.7 19.3 50.8-.5 70.6l35.4 35.3H0" />
+              </svg>
+            </div>
+            <div  className='check'>
+              <div className='filt'>
                 <input type='checkbox' className='assa' />
                 <label htmlFor='assa'>assassin</label>
               </div>
@@ -33,6 +56,19 @@ const Characters = () => {
           </div>
           <div className='champs'>
 
+            {champs && champs.map((p) => (
+              <button className='card'>
+                <div className='over'>
+                  <img src={p.icon} />
+                  <div className='dark'>
+
+                  </div>
+                  <div className='lay'>
+                    <p>{p.name}</p>
+                  </div>
+                </div>
+              </button>
+            ))}
           </div>
         </div>
       </div>
--- layihe/server/router/champions.js
+++ layihe/server/router/champions.js
@@ -2,10 +2,10 @@ const express = require("express");
 const { getAllChampions, getChampionsById, deleteChampions, editChampions, postChampions } = require("../controller/champions");
 const routerc = express.Router();
 
-router.get("/", getAllChampions);
-router.get("/:id", getChampionsById);
-router.delete("/:id", deleteChampions);
-router.put("/:id", editChampions);
-router.post("/", postChampions);
+routerc.get("/", getAllChampions);
+routerc.get("/:id", getChampionsById);
+routerc.delete("/:id", deleteChampions);
+routerc.put("/:id", editChampions);
+routerc.post("/", postChampions);
 
 module.exports = routerc;
