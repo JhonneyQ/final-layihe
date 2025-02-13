837b13bc6f2458c6556978431ed1a991d0fc7cdf
JhonneyQ <kananqadirov2005@gmail.com>
2025-02-13 19:17:42 +0400

    details sehifesinin 90% hazirdir

--- layihe/front/src/layouts/header/index.jsx
+++ layihe/front/src/layouts/header/index.jsx
@@ -7,28 +7,30 @@ const Cheader = () => {
     return (
         <header>
             <div className='container'>
-                <SiLeagueoflegends className='icon' />
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/">HOME</Link>
-                        </li>
-                        <li>
-                            <Link to="/profile">PROFILE</Link>
-                        </li>
-                        <li>
-                            <Link to="/moments">MOMENTS</Link>
-                        </li>
-                        <li>
-                            <Link to="/characters">CHARACTERS</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">ABOUT US</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <a class="play-button" href="https://www.leagueoflegends.com/tr-tr/" >Play</a>
+                <div className='all'>
+                    <SiLeagueoflegends className='icon' />
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">HOME</Link>
+                            </li>
+                            <li>
+                                <Link to="/profile">PROFILE</Link>
+                            </li>
+                            <li>
+                                <Link to="/moments">MOMENTS</Link>
+                            </li>
+                            <li>
+                                <Link to="/characters">CHARACTERS</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">ABOUT US</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <a class="play-button" href="https://www.leagueoflegends.com/tr-tr/" >Play</a>
 
+                </div>
             </div>
         </header>
     )
--- layihe/front/src/pages/characterDetails/index.jsx
+++ layihe/front/src/pages/characterDetails/index.jsx
@@ -1,8 +1,102 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./index.scss"
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const CharDetails = () => {
+  const { id } = useParams()
+  const [card, setCard] = useState([])
+  const [stat, setStat] = useState([])
+  const [skills, setSkills] = useState([])
+  console.log(id);
+
+
+
+  const getData = async () => {
+    try {
+      const res = await axios(`http://localhost:8080/api/champions/${id}`)
+      console.log(res.data.data);
+      setStat(res.data.data.stats[0])
+      setCard(res.data.data)
+      setSkills(res.data.data.skills)
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+  
+  
+
+
+
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+
+
+
+
   return (
-    <div>CharDetails</div>
+    <section className="details" style={{ position: "relative", overflow: "hidden" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          background: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), 
+                   url('${card.background}') 
+                   center/cover no-repeat`,
+          filter: "blur(10px)",
+          zIndex: -1,
+        }}
+      ></div>
+
+      <div className="container">
+        <div className="card">
+          <div className="par">
+            <div className='first'>
+              <p>{card.title} / {card.type}</p>
+              <h2>{card.name}</h2>
+              <span>{card.description}</span>
+              <div className='about'>
+                <div className='stat'>
+                  <div className='hp'>
+                    <p>HP</p>
+                    <span>{stat.hp}</span>
+                  </div>
+                  <div className='armor'>
+                    <p>ARMOR</p>
+                    <span>{stat.armor}</span>
+                  </div>
+                  <div className='mr'>
+                    <p>MR</p>
+                    <span>{stat.mr}</span>
+                  </div>
+                </div>
+                <div className='skill'>
+                  <img src={skills[0]}/>
+                  <img src={skills[1]}/>
+                  <img src={skills[2]}/>
+                  <img src={skills[3]}/>
+                </div>
+              </div>
+            </div>
+            {/* <div className='icon'>
+              <p>{card.type}</p>
+            </div> */}
+          </div>
+          <div className="pic">
+            <img src={card.image}/>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
   )
 }
 
